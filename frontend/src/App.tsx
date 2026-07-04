f922f32acc337dbc41db55a38b5291a1724a0c4e
AounFarani <aounfarani7@gmail.com>
2026-07-04 23:20:47 +0500

    shadcn ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
       <h1 className="text-red-500 text-5xl">Project Initialization</h1>
+      <Button variant="ghost">Click me</Button>
     </>
   )
 }
